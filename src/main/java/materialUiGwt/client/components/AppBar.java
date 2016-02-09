03ebb9d37b4a1b84abdcc6d86f4802ecddf1ed6d
Chris Jones <chris@axismedtech.com>
2016-02-09 15:07:14 -0700

    wip

--- src/main/java/materialUiGwt/client/components/AppBar.java
+++ src/main/java/materialUiGwt/client/components/AppBar.java
@@ -1,4 +1,8 @@
 package materialUiGwt.client.components;
 
 public class AppBar {
+
+    public static class Props {
+
+    }
 }
---
+++ src/main/java/materialUiGwt/client/components/RaisedButton.java
@@ -0,0 +1,19 @@
+package materialUiGwt.client.components;
+
+import jsinterop.annotations.JsType;
+import webmattr.react.ReactElement;
+
+public class RaisedButton {
+    @JsType
+    public static class Props {
+        String backgroundColor;
+        String className;
+        boolean disabled;
+        String disabledBackgroundColor;
+        String disabledLabelColor;
+        ReactElement label;
+        String labelStyle; // ?
+        boolean linkButton;
+        Object onMouseDown; // func
+    }
+}
