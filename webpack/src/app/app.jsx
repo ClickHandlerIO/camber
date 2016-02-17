954d5fe1cb14adc11aacb5acfd846bbff35859dc
Chris Jones <chris@axismedtech.com>
2016-02-17 13:48:52 -0700

    add webpack project

---
+++ webpack/src/app/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactRouter from 'react-router';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import RaisedButton from 'material-ui/lib/raised-button';
+
+module.exports = {
+  React: React,
+  ReactDOM: ReactDOM,
+  ReactRouter: ReactRouter,
+  InjectTapEventPlugin: injectTapEventPlugin,
+  RaisedButton: RaisedButton,
+};
