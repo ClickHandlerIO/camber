0471f29d9f5f6a7e35e9b298b237ee3471ad44f4
Chris Jones <chris@axismedtech.com>
2016-02-19 08:00:37 -0700

    wip

--- src/main/java/camber/client/materialUi/Avatar.java
+++ src/main/java/camber/client/materialUi/Avatar.java
@@ -1,18 +1,33 @@
 package camber.client.materialUi;
 
+import camber.client.ExternalComponent;
+import camber.client.ReactClass;
+import io.clickhandler.web.dom.CSSProps;
+import io.clickhandler.web.react.BaseProps;
 import jsinterop.annotations.JsType;
 import io.clickhandler.web.react.ReactElement;
 
-public class Avatar {
-    @JsType
-    public static class Props {
-        String backgroundColor;
-        String className;
-        String color;
-        ReactElement icon;
-        int size; // default 40
-        String src;
-        String style; //?
+import javax.inject.Inject;
 
+public class Avatar extends ExternalComponent<Avatar.Props> {
+
+    @Inject
+    public Avatar() {
+    }
+
+    @Override
+    protected native ReactClass<Props> reactClass() /*-{
+        return $wnd.Camber.MaterialUi.Avatar;
+    }-*/;
+
+    @JsType
+    public static class Props extends BaseProps{
+       public String backgroundColor;
+       public String className;
+       public String color;
+       public ReactElement icon;
+       public int size;
+       public String src;
+       public CSSProps style;
     }
 }
--- webpack/src/app.jsx
+++ webpack/src/app.jsx
@@ -5,15 +5,19 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 
 // Material Ui
 import AppBar from 'material-ui/lib/app-bar';
+import Avatar from 'material-ui/lib/avatar';
 import Colors from 'material-ui/lib/styles/colors';
 import ColorManipulator from 'material-ui/lib/utils/color-manipulator';
 import Divider from 'material-ui/lib/divider';
 import FlatButton from 'material-ui/lib/flat-button';
 import IconButton from 'material-ui/lib/icon-button';
+import IconMenu from 'material-ui/lib/menus/icon-menu';
 import LeftNav from 'material-ui/lib/left-nav';
 import Launch from 'material-ui/lib/svg-icons/action/launch';
 import List from 'material-ui/lib/lists/list';
 import ListItem from 'material-ui/lib/lists/list-item';
+import Menu from 'material-ui/lib/menu/menu';
+import MenuItem from 'material-ui/lib/menu/menu-item'; // TODO 2 menu item (one in menus too)
 import MuiTheme from 'material-ui/lib/styles/getMuiTheme';
 import Paper from 'material-ui/lib/paper';
 import Spacing from 'material-ui/lib/styles/spacing';
@@ -32,12 +36,16 @@ module.exports = {
   InjectTapEventPlugin: injectTapEventPlugin,
   MaterialUi: {
     AppBar: AppBar,
+    Avatar: Avatar,
     Divider: Divider,
     FlatButton: FlatButton,
     IconButton: IconButton,
+    IconMenu: IconMenu,
     LeftNav: LeftNav,
     List: List,
     ListItem: ListItem,
+    Menu: Menu,
+    MenuItem: MenuItem,
     Paper: Paper,
     RaisedButton: RaisedButton,
     SvgIcon: SvgIcon,
