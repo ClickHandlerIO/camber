7dfa22771aa8aa4565a203e5d0a45685dff24d50
Chris Jones <chris@axismedtech.com>
2016-02-24 12:01:16 -0700

    floating action button

--- src/main/java/camber/client/materialUi/FloatingActionButton.java
+++ src/main/java/camber/client/materialUi/FloatingActionButton.java
@@ -4,14 +4,26 @@ import io.clickhandler.web.dom.CSSProps;
 import io.clickhandler.web.event.MouseEventHandler;
 import io.clickhandler.web.event.TouchEventHandler;
 import io.clickhandler.web.react.BaseProps;
+import io.clickhandler.web.react.ExternalComponent;
+import io.clickhandler.web.react.ReactClass;
 import jsinterop.annotations.JsOverlay;
 import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
-public class FloatingActionButton {
+public class FloatingActionButton extends ExternalComponent<FloatingActionButton.Props> {
+
+    @Inject
+    public FloatingActionButton() {
+    }
+
+    @Override
+    protected native ReactClass<Props> reactClass() /*-{
+        return $wnd.Camber.MaterialUi.FloatingActionButton;
+    }-*/;
 
     @JsType(isNative = true)
     public interface Props extends BaseProps {
