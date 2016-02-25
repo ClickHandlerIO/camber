169f3fbf8aa43a1110618a9076802602285fe502
Oliver <oliver@axismedtech.com>
2016-02-25 11:25:56 -0800

    updating Camber components, all table stuff

--- src/main/java/camber/client/materialUi/TableFooter.java
+++ src/main/java/camber/client/materialUi/TableFooter.java
@@ -1,5 +1,12 @@
 package camber.client.materialUi;
 
+import io.clickhandler.web.dom.CSSProps;
+import io.clickhandler.web.event.MouseEventHandler;
+import io.clickhandler.web.react.BaseProps;
+import io.clickhandler.web.react.ExternalComponent;
+import io.clickhandler.web.react.ReactClass;
+import jsinterop.annotations.JsOverlay;
+import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
 import javax.inject.Inject;
@@ -9,15 +16,77 @@ import javax.inject.Singleton;
  *
  */
 @Singleton
-public class TableFooter {
-    @JsType
-    public static class Props {
-        boolean adjustForCheckbox;
-        String className;
-        String style;
+public class TableFooter extends ExternalComponent<TableFooter.Props> {
 
-        @Inject
-        public Props() {
+    @Inject
+    public TableFooter() {
+    }
+
+    @Override
+    protected native ReactClass<Props> reactClass() /*-{
+        return $wnd.Camber.MaterialUi.TableFooter;
+    }-*/;
+
+    @JsType(isNative = true)
+    public interface Props extends BaseProps {
+//        boolean adjustForCheckbox;
+//        String className;
+//        CSSProps style;
+//        MouseEventHandler onClick;
+
+        @JsProperty
+        boolean isAdjustForCheckbox();
+
+        @JsProperty
+        void setAdjustForCheckbox(boolean adjustForCheckbox);
+
+        @JsProperty
+        String getClassName();
+
+        @JsProperty
+        void setClassName(String className);
+
+        @JsProperty
+        CSSProps getStyle();
+
+        @JsProperty
+        void setStyle(CSSProps style);
+
+        @JsProperty
+        MouseEventHandler getOnClick();
+
+        @JsProperty
+        void setOnClick(MouseEventHandler onClick);
+
+
+        ////////////////////
+        // fluent setters
+        ////////////////////
+
+        @JsOverlay
+        default Props adjustForCheckbox(final boolean adjustForCheckbox) {
+            setAdjustForCheckbox(adjustForCheckbox);
+            return this;
         }
+
+        @JsOverlay
+        default Props className(final String className) {
+            setClassName(className);
+            return this;
+        }
+
+        @JsOverlay
+        default Props style(final CSSProps style) {
+            setStyle(style);
+            return this;
+        }
+
+        @JsOverlay
+        default Props onClick(final MouseEventHandler onClick) {
+            setOnClick(onClick);
+            return this;
+        }
+
+
     }
 }
