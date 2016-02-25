2f10ffacd2dfea31c7995c85b6523d8d59213d73
Oliver <oliver@axismedtech.com>
2016-02-24 23:47:58 -0800

    updating Camber components, minor changes to test

--- src/main/java/camber/client/materialUi/TimePicker.java
+++ src/main/java/camber/client/materialUi/TimePicker.java
@@ -1,5 +1,9 @@
 package camber.client.materialUi;
 
+import io.clickhandler.web.Func;
+import io.clickhandler.web.dom.CSSProps;
+import io.clickhandler.web.event.FocusEventHandler;
+import io.clickhandler.web.event.TouchEventHandler;
 import jsinterop.annotations.JsType;
 
 import javax.inject.Inject;
@@ -7,24 +11,27 @@ import javax.inject.Singleton;
 
 @Singleton
 public class TimePicker {
-    @JsType
+
+    @Inject
+    public TimePicker() {
+    }
+
+
+
+    @JsType(isNative = true)
     public static class Props {
         boolean autoOk;
         String defaultTime;
         String format; // enum 'ampm '24hr'
         boolean pedantic;
-        String style; // ?
-        String textFieldStyle; // ?
+        CSSProps style; // ?
+        CSSProps textFieldStyle; // ?
+        Func.Run onChange; // func
+        Func.Run onDismiss; // func
+        FocusEventHandler onFocus; // func
+        Func.Run onShow; // func
+        TouchEventHandler onTouchTap; // func
 
-        // functions
-        Object onChange; // func
-        Object onDismiss; // func
-        Object onFocus; // func
-        Object onShow; // func
-        Object onTouchTap; // func
 
-        @Inject
-        public Props() {
-        }
     }
 }
--- src/main/java/camber/client/materialUi/Toggle.java
+++ src/main/java/camber/client/materialUi/Toggle.java
@@ -1,5 +1,7 @@
 package camber.client.materialUi;
 
+import io.clickhandler.web.Func;
+import io.clickhandler.web.dom.CSSProps;
 import jsinterop.annotations.JsType;
 
 import javax.inject.Inject;
@@ -10,26 +12,29 @@ import javax.inject.Singleton;
  */
 @Singleton
 public class Toggle {
-    @JsType
+
+    @Inject
+    public Toggle() {
+    }
+
+
+
+    @JsType(isNative = true)
     public static class Props {
         boolean defaultToggled;
         boolean disabled;
-        String elementStyle;
-        String iconStyle;
+        CSSProps elementStyle;
+        CSSProps iconStyle;
         String labelPosition; // enum 'left' 'right' default left
-        String labelStyle;
-        String rippleStyle;
-        String style;
+        CSSProps labelStyle;
+        CSSProps rippleStyle;
+        CSSProps style;
         String thumbStyle;
         boolean toggled; // toggled if set to true
-        String trackStyle;
+        CSSProps trackStyle;
         String valueLink;
+        Func.Run onToggle; // func
 
-        // functions
-        Object onToggle; // func
 
-        @Inject
-        public Props() {
-        }
     }
 }
