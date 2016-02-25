be54e77d4ecec89b59a675e00d561e21b87d2b16
Oliver <oliver@axismedtech.com>
2016-02-25 11:58:07 -0800

    updating Camber components, toolbar stuff

--- src/main/java/camber/client/materialUi/ToolbarTitle.java
+++ src/main/java/camber/client/materialUi/ToolbarTitle.java
@@ -1,5 +1,12 @@
 package camber.client.materialUi;
 
+import io.clickhandler.web.dom.CSSProps;
+import io.clickhandler.web.event.MouseEventHandler;
+import io.clickhandler.web.react.BaseProps;
+import io.clickhandler.web.react.ExternalComponent;
+import io.clickhandler.web.react.ReactClass;
+import jsinterop.annotations.JsOverlay;
+import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
 import javax.inject.Inject;
@@ -9,15 +16,77 @@ import javax.inject.Singleton;
  *
  */
 @Singleton
-public class ToolbarTitle {
-    @JsType
-    public static class Props {
-        String className;
-        String style;
-        String text; // text to be displayed in title
+public class ToolbarTitle extends ExternalComponent<ToolbarTitle.Props> {
 
-        @Inject
-        public Props() {
+    @Inject
+    public ToolbarTitle() {
+    }
+
+    @Override
+    protected native ReactClass<Props> reactClass() /*-{
+        return $wnd.Camber.MaterialUi.ToolbarTitle;
+    }-*/;
+
+    @JsType(isNative = true)
+    public interface Props extends BaseProps {
+//        String className;
+//        CSSProps style;
+//        String text; // text to be displayed in title
+//        MouseEventHandler onClick;
+
+        @JsProperty
+        String getClassName();
+
+        @JsProperty
+        void setClassName(String className);
+
+        @JsProperty
+        CSSProps getStyle();
+
+        @JsProperty
+        void setStyle(CSSProps style);
+
+        @JsProperty
+        String getText();
+
+        @JsProperty
+        void setText(String text);
+
+        @JsProperty
+        MouseEventHandler getOnClick();
+
+        @JsProperty
+        void setOnClick(MouseEventHandler onClick);
+
+
+        ////////////////////
+        // fluent setters
+        ////////////////////
+
+        @JsOverlay
+        default Props className(final String className) {
+            setClassName(className);
+            return this;
         }
+
+        @JsOverlay
+        default Props style(final CSSProps style) {
+            setStyle(style);
+            return this;
+        }
+
+        @JsOverlay
+        default Props text(final String text) {
+            setText(text);
+            return this;
+        }
+
+        @JsOverlay
+        default Props onClick(final MouseEventHandler onClick) {
+            setOnClick(onClick);
+            return this;
+        }
+
+
     }
 }
