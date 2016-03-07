83739bfc63da397f25dc610c6f6bb2f7a0515373
Oliver <oliver@axismedtech.com>
2016-03-07 13:55:21 -0800

    finish a couple of charts

--- src/main/java/camber/client/reactd3/LineChart.java
+++ src/main/java/camber/client/reactd3/LineChart.java
@@ -3,38 +3,45 @@ package camber.client.reactd3;
 
 import io.clickhandler.web.event.MouseEventHandler;
 import io.clickhandler.web.react.BaseProps;
+import io.clickhandler.web.react.ExternalComponent;
+import io.clickhandler.web.react.ReactClass;
+import jsinterop.annotations.JsOverlay;
 import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
 import javax.inject.Inject;
 
-public class LineChart {
+public class LineChart extends ExternalComponent<LineChart.Props> {
 
     @Inject
     public LineChart() {
     }
 
+    @Override
+    protected native ReactClass<Props> reactClass() /*-{
+        return $wnd.Camber.LineChart;
+    }-*/;
 
 
     @JsType(isNative = true)
-    public class Props extends BaseProps {
-        Object legend; // usually a string, assuming like legend Template in chartjs, sometimes boolean
-        LineData[] data;
-        String className;
-        Object width; // string or double
-        Object height;
-        ViewBoxObject viewBoxObject;
-        String title;
-        String yAxisLabel;
-        String xAxisLabel;
-        String xAxisClassName;
-        String yAxisClassName;
-        boolean gridHorizontal;
-        double circleRadius;
-        boolean hoverAnimation;
-        Object margins; // they use an object for margins
-
-        MouseEventHandler onClick;
+    public interface Props extends BaseProps {
+//        Object legend; // usually a string, assuming like legend Template in chartjs, sometimes boolean
+//        LineData[] data;
+//        String className;
+//        Object width; // string or double
+//        Object height;
+//        ViewBoxObject viewBoxObject;
+//        String title;
+//        String yAxisLabel;
+//        String xAxisLabel;
+//        String xAxisClassName;
+//        String yAxisClassName;
+//        boolean gridHorizontal;
+//        double circleRadius;
+//        boolean hoverAnimation;
+//        Object margins; // they use an object for margins
+//
+//        MouseEventHandler onClick;
 
 
         @JsProperty
@@ -138,82 +145,98 @@ public class LineChart {
         // fluent setters
         ////////////////////
 
-        public Props legend(final Object legend) {
-            this.legend = legend;
+        @JsOverlay
+        default Props legend(final Object legend) {
+            setLegend(legend);
             return this;
         }
 
-        public Props data(final LineData[] data) {
-            this.data = data;
+        @JsOverlay
+        default Props data(final LineData[] data) {
+            setData(data);
             return this;
         }
 
-        public Props className(final String className) {
-            this.className = className;
+        @JsOverlay
+        default Props className(final String className) {
+            setClassName(className);
             return this;
         }
 
-        public Props width(final String width) {
-            this.width = width;
+        @JsOverlay
+        default Props width(final String width) {
+            setWidth(width);
             return this;
         }
 
-        public Props height(final String height) {
-            this.height = height;
+        @JsOverlay
+        default Props height(final String height) {
+            setHeight(height);
             return this;
         }
 
-        public Props viewBoxObject(final ViewBoxObject viewBoxObject) {
-            this.viewBoxObject = viewBoxObject;
+        @JsOverlay
+        default Props viewBoxObject(final ViewBoxObject viewBoxObject) {
+            setViewBoxObject(viewBoxObject);
             return this;
         }
 
-        public Props title(final String title) {
-            this.title = title;
+        @JsOverlay
+        default Props title(final String title) {
+            setTitle(title);
             return this;
         }
 
-        public Props yAxisLabel(final String yAxisLabel) {
+        @JsOverlay
+        default Props yAxisLabel(final String yAxisLabel) {
             setyAxisLabel(yAxisLabel);
             return this;
         }
 
-        public Props xAxisLabel(final String xAxisLabel) {
+        @JsOverlay
+        default Props xAxisLabel(final String xAxisLabel) {
             setxAxisLabel(xAxisLabel);
             return this;
         }
 
-        public Props xAxisClassName(final String xAxisClassName) {
+        @JsOverlay
+        default Props xAxisClassName(final String xAxisClassName) {
             setxAxisClassName(xAxisClassName);
             return this;
         }
 
-        public Props yAxisClassName(final String yAxisClassName) {
+        @JsOverlay
+        default Props yAxisClassName(final String yAxisClassName) {
             setyAxisClassName(yAxisClassName);
             return this;
         }
 
-        public Props gridHorizontal(final boolean gridHorizontal) {
+        @JsOverlay
+        default Props gridHorizontal(final boolean gridHorizontal) {
             setGridHorizontal(gridHorizontal);
             return this;
         }
 
-        public Props circleRadius(final double circleRadius) {
+        @JsOverlay
+        default Props circleRadius(final double circleRadius) {
             setCircleRadius(circleRadius);
             return this;
         }
 
-        public Props hoverAnimation(final boolean hoverAnimation) {
+        @JsOverlay
+        default Props hoverAnimation(final boolean hoverAnimation) {
             setHoverAnimation(hoverAnimation);
             return this;
         }
 
-        public Props margin(final Object margin) {
-            setMargin(margin);
+        @JsOverlay
+        default Props margins(final Object margins) {
+            setMargins(margins);
             return this;
         }
 
-        public Props onClick(final MouseEventHandler onClick) {
+        @JsOverlay
+        default Props onClick(final MouseEventHandler onClick) {
             setOnClick(onClick);
             return this;
         }
@@ -224,27 +247,149 @@ public class LineChart {
 
 
     @JsType(isNative = true)
-    public class LineData {
-        String name;
-        Values[] values;
-        double strokeWidth;
-        String strokeDashArray;
+    public interface LineData {
+//        String name;
+//        Values[] values;
+//        double strokeWidth;
+//        String strokeDashArray;
+
+        @JsProperty
+        String getName();
+
+        @JsProperty
+        void setName(String name);
+
+        @JsProperty
+        Values[] getValues();
+
+        @JsProperty
+        void setValues(Values[] values);
+
+        @JsProperty
+        double getStrokeWidth();
+
+        @JsProperty
+        void setStrokeWidth(double strokeWidth);
+
+        @JsProperty
+        String getStrokeDashArray();
+
+        @JsProperty
+        void setStrokeDashArray(String strokeDashArray);
+
+        @JsOverlay
+        default LineData name(final String name) {
+            setName(name);
+            return this;
+        }
+
+        @JsOverlay
+        default LineData values(final Values[] values) {
+            setValues(values);
+            return this;
+        }
+
+        @JsOverlay
+        default LineData strokeWidth(final double strokeWidth) {
+            setStrokeWidth(strokeWidth);
+            return this;
+        }
+
+        @JsOverlay
+        default LineData strokeDashArray(final String strokeDashArray) {
+            setStrokeDashArray(strokeDashArray);
+            return this;
+        }
+
 
     }
 
     @JsType(isNative = true)
-    public class Values {
-        double x;
-        double y;
+    public interface Values {
+//        double x;
+//        double y;
+
+        @JsProperty
+        double getX();
+
+        @JsProperty
+        void setX(double x);
+
+        @JsProperty
+        double getY();
+
+        @JsProperty
+        void setY(double y);
+
+        @JsOverlay
+        default Values x(final double x) {
+            setX(x);
+            return this;
+        }
+
+        @JsOverlay
+        default Values y(final double y) {
+            setY(y);
+            return this;
+        }
+
 
     }
 
     @JsType(isNative = true)
-    public class ViewBoxObject {
-        double x;
-        double y;
-        double width;
-        double height;
+    public interface ViewBoxObject {
+//        double x;
+//        double y;
+//        double width;
+//        double height;
+
+        @JsProperty
+        double getX();
+
+        @JsProperty
+        void setX(double x);
+
+        @JsProperty
+        double getY();
+
+        @JsProperty
+        void setY(double y);
+
+        @JsProperty
+        double getWidth();
+
+        @JsProperty
+        void setWidth(double width);
+
+        @JsProperty
+        double getHeight();
+
+        @JsProperty
+        void setHeight(double height);
+
+        @JsOverlay
+        default ViewBoxObject x(final double x) {
+            setX(x);
+            return this;
+        }
+
+        @JsOverlay
+        default ViewBoxObject y(final double y) {
+            setY(y);
+            return this;
+        }
+
+        @JsOverlay
+        default ViewBoxObject width(final double width) {
+            setWidth(width);
+            return this;
+        }
+
+        @JsOverlay
+        default ViewBoxObject height(final double height) {
+            setHeight(height);
+            return this;
+        }
 
     }
 
--- src/main/java/camber/client/reactd3/Treemap.java
+++ src/main/java/camber/client/reactd3/Treemap.java
@@ -4,49 +4,213 @@ package camber.client.reactd3;
 import io.clickhandler.web.Func;
 import io.clickhandler.web.event.MouseEventHandler;
 import io.clickhandler.web.react.BaseProps;
+import io.clickhandler.web.react.ExternalComponent;
+import io.clickhandler.web.react.ReactClass;
+import jsinterop.annotations.JsOverlay;
+import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
 
-public class Treemap {
+@Singleton
+public class Treemap extends ExternalComponent<Treemap.Props> {
 
     @Inject
     public Treemap() {
     }
 
+    @Override
+    protected native ReactClass<Props> reactClass() /*-{
+        return $wnd.Camber.Treemap;
+    }-*/;
 
 
     @JsType(isNative = true)
-    public class Props extends BaseProps {
-        TreemapData[] data;
-        String title;
-        Object fontSize; // can be String or double
-        String textColor;
-        double width;
-        double height;
-        boolean hoverAnimation;
-        Object margins; // can be created as another interface in here if needed
+    public interface Props extends BaseProps {
+//        TreemapData[] data;
+//        String title;
+//        Object fontSize; // can be String or double
+//        String textColor;
+//        double width;
+//        double height;
+//        boolean hoverAnimation;
+//        Object margins; // can be created as another interface in here if needed
+//
+//        Func.Run colors;
+//        Func.Run2<Object, Object> colorAccessor;
+//        MouseEventHandler onClick;
 
-        Func.Run colors;
-        Func.Run2<Object, Object> colorAccessor;
-        MouseEventHandler onClick;
-        
 
+        @JsProperty
+        TreemapData[] getData();
+
+        @JsProperty
+        void setData(TreemapData[] data);
+
+        @JsProperty
+        String getTitle();
+
+        @JsProperty
+        void setTitle(String title);
+
+        @JsProperty
+        Object getFontSize();
+
+        @JsProperty
+        void setFontSize(Object fontSize);
+
+        @JsProperty
+        String getTextColor();
+
+        @JsProperty
+        void setTextColor(String textColor);
+
+        @JsProperty
+        double getWidth();
+
+        @JsProperty
+        void setWidth(double width);
+
+        @JsProperty
+        double getHeight();
+
+        @JsProperty
+        void setHeight(double height);
+
+        @JsProperty
+        boolean isHoverAnimation();
+
+        @JsProperty
+        void setHoverAnimation(boolean hoverAnimation);
+
+        @JsProperty
+        Object getMargins();
+
+        @JsProperty
+        void setMargins(Object margins);
+
+        @JsProperty
+        Func.Run getColors();
+
+        @JsProperty
+        void setColors(Func.Run colors);
+
+        @JsProperty
+        Func.Run2<Object, Object> getColorAccessor();
+
+        @JsProperty
+        void setColorAccessor(Func.Run2<Object, Object> colorAccessor);
+
+        @JsProperty
+        MouseEventHandler getOnClick();
+
+        @JsProperty
+        void setOnClick(MouseEventHandler onClick);
 
 
         ////////////////////
         // fluent setters
         ////////////////////
 
+        @JsOverlay
+        default Props data(final TreemapData[] data) {
+            setData(data);
+            return this;
+        }
+
+        @JsOverlay
+        default Props title(final String title) {
+            setTitle(title);
+            return this;
+        }
+
+        @JsOverlay
+        default Props fontSize(final Object fontSize) {
+            setFontSize(fontSize);
+            return this;
+        }
+
+        @JsOverlay
+        default Props textColor(final String textColor) {
+            setTextColor(textColor);
+            return this;
+        }
+
+        @JsOverlay
+        default Props width(final double width) {
+            setWidth(width);
+            return this;
+        }
+
+        @JsOverlay
+        default Props height(final double height) {
+            setHeight(height);
+            return this;
+        }
+
+        @JsOverlay
+        default Props hoverAnimation(final boolean hoverAnimation) {
+            setHoverAnimation(hoverAnimation);
+            return this;
+        }
+
+        @JsOverlay
+        default Props margins(final Object margins) {
+            setMargins(margins);
+            return this;
+        }
+
+        @JsOverlay
+        default Props colors(final Func.Run colors) {
+            setColors(colors);
+            return this;
+        }
+
+        @JsOverlay
+        default Props colorAccessor(final Func.Run2<Object, Object> colorAccessor) {
+            setColorAccessor(colorAccessor);
+            return this;
+        }
+
+        @JsOverlay
+        default Props onClick(final MouseEventHandler onClick) {
+            setOnClick(onClick);
+            return this;
+        }
 
 
     }
 
 
     @JsType(isNative = true)
-    public class TreemapData {
-        String label;
-        double value;
+    public interface TreemapData {
+//        String label;
+//        double value;
+
+        @JsProperty
+        String getLabel();
+
+        @JsProperty
+        void setLabel(String label);
+
+        @JsProperty
+        double getValue();
+
+        @JsProperty
+        void setValue(double value);
+
+        @JsOverlay
+        default TreemapData label(final String label) {
+            setLabel(label);
+            return this;
+        }
+
+        @JsOverlay
+        default TreemapData value(final double value) {
+            setValue(value);
+            return this;
+        }
 
     }
 
