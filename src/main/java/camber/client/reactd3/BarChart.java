22f0f58321863b7a1cdc7716b33fff5dc3b7ad50
Oliver <oliver@axismedtech.com>
2016-03-07 14:30:36 -0800

    expand on bar chart and pie chaart

--- src/main/java/camber/client/reactd3/BarChart.java
+++ src/main/java/camber/client/reactd3/BarChart.java
@@ -4,61 +4,307 @@ package camber.client.reactd3;
 import io.clickhandler.web.Func;
 import io.clickhandler.web.event.MouseEventHandler;
 import io.clickhandler.web.react.BaseProps;
+import io.clickhandler.web.react.ExternalComponent;
+import io.clickhandler.web.react.ReactClass;
+import jsinterop.annotations.JsOverlay;
+import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
 
-public class BarChart {
+@Singleton
+public class BarChart extends ExternalComponent<BarChart.Props> {
 
     @Inject
     public BarChart() {
     }
 
-
+    @Override
+    protected native ReactClass<Props> reactClass() /*-{
+        return $wnd.Camber.BarChart;
+    }-*/;
 
     @JsType(isNative = true)
-    public class Props extends BaseProps {
-        String chartClassName;
-        String xAxisClassName;
-        String yAxisClassName;
-        BarData[] data;
-        String title;
-        double yAxisTickCount;
-        String yAxisLabel;
-        String xAxisLabel;
-        String stackOffset; // one of 4 values
-        Object width; // string or double
-        double height;
-        double rangeRoundBandsPadding;
-        Object margins; // can be created as another interface in here if needed
-        String fill; // seems to be fill color
+    public interface Props extends BaseProps {
+//        String chartClassName;
+//        String xAxisClassName;
+//        String yAxisClassName;
+//        BarData[] data;
+//        String title;
+//        double yAxisTickCount;
+//        String yAxisLabel;
+//        String xAxisLabel;
+//        String stackOffset; // one of 4 values
+//        Object width; // string or double
+//        double height;
+//        double rangeRoundBandsPadding;
+//        Object margins; // can be created as another interface in here if needed
+//        String fill; // seems to be fill color
+//        Func.Run1<Object> valuesAccessor;
+//        MouseEventHandler onClick;
 
-        Func.Run1<Object> valuesAccessor;
-        MouseEventHandler onClick;
 
+        @JsProperty
+        String getChartClassName();
 
+        @JsProperty
+        void setChartClassName(String chartClassName);
+
+        @JsProperty
+        String getxAxisClassName();
+
+        @JsProperty
+        void setxAxisClassName(String xAxisClassName);
+
+        @JsProperty
+        String getyAxisClassName();
+
+        @JsProperty
+        void setyAxisClassName(String yAxisClassName);
+
+        @JsProperty
+        BarData[] getData();
+
+        @JsProperty
+        void setData(BarData[] data);
+
+        @JsProperty
+        String getTitle();
+
+        @JsProperty
+        void setTitle(String title);
+
+        @JsProperty
+        double getyAxisTickCount();
+
+        @JsProperty
+        void setyAxisTickCount(double yAxisTickCount);
+
+        @JsProperty
+        String getyAxisLabel();
+
+        @JsProperty
+        void setyAxisLabel(String yAxisLabel);
+
+        @JsProperty
+        String getxAxisLabel();
+
+        @JsProperty
+        void setxAxisLabel(String xAxisLabel);
+
+        @JsProperty
+        String getStackOffset();
+
+        @JsProperty
+        void setStackOffset(String stackOffset);
+
+        @JsProperty
+        Object getWidth();
+
+        @JsProperty
+        void setWidth(Object width);
+
+        @JsProperty
+        double getHeight();
+
+        @JsProperty
+        void setHeight(double height);
+
+        @JsProperty
+        double getRangeRoundBandsPadding();
+
+        @JsProperty
+        void setRangeRoundBandsPadding(double rangeRoundBandsPadding);
+
+        @JsProperty
+        Object getMargins();
+
+        @JsProperty
+        void setMargins(Object margins);
+
+        @JsProperty
+        String getFill();
+
+        @JsProperty
+        void setFill(String fill);
+
+        @JsProperty
+        Func.Run1<Object> getValuesAccessor();
+
+        @JsProperty
+        void setValuesAccessor(Func.Run1<Object> valuesAccessor);
+
+        @JsProperty
+        MouseEventHandler getOnClick();
+
+        @JsProperty
+        void setOnClick(MouseEventHandler onClick);
 
 
         ////////////////////
         // fluent setters
         ////////////////////
 
+        @JsOverlay
+        default Props chartClassName(final String chartClassName) {
+            setChartClassName(chartClassName);
+            return this;
+        }
+
+        @JsOverlay
+        default Props xAxisClassName(final String xAxisClassName) {
+            setxAxisClassName(xAxisClassName);
+            return this;
+        }
+
+        @JsOverlay
+        default Props yAxisClassName(final String yAxisClassName) {
+            setyAxisClassName(yAxisClassName);
+            return this;
+        }
+
+        @JsOverlay
+        default Props data(final BarData[] data) {
+            setData(data);
+            return this;
+        }
+
+        @JsOverlay
+        default Props title(final String title) {
+            setTitle(title);
+            return this;
+        }
+
+        @JsOverlay
+        default Props yAxisTickCount(final double yAxisTickCount) {
+            setyAxisTickCount(yAxisTickCount);
+            return this;
+        }
+
+        @JsOverlay
+        default Props yAxisLabel(final String yAxisLabel) {
+            setyAxisLabel(yAxisLabel);
+            return this;
+        }
+
+        @JsOverlay
+        default Props xAxisLabel(final String xAxisLabel) {
+            setxAxisLabel(xAxisLabel);
+            return this;
+        }
+
+        @JsOverlay
+        default Props stackOffset(final String stackOffset) {
+            setStackOffset(stackOffset);
+            return this;
+        }
+
+        @JsOverlay
+        default Props width(final Object width) {
+            setWidth(width);
+            return this;
+        }
+
+        @JsOverlay
+        default Props height(final double height) {
+            setHeight(height);
+            return this;
+        }
+
+        @JsOverlay
+        default Props rangeRoundBandsPadding(final double rangeRoundBandsPadding) {
+            setRangeRoundBandsPadding(rangeRoundBandsPadding);
+            return this;
+        }
+
+        @JsOverlay
+        default Props margins(final Object margins) {
+            setMargins(margins);
+            return this;
+        }
+
+        @JsOverlay
+        default Props fill(final String fill) {
+            setFill(fill);
+            return this;
+        }
+
+        @JsOverlay
+        default Props valuesAccessor(final Func.Run1<Object> valuesAccessor) {
+            setValuesAccessor(valuesAccessor);
+            return this;
+        }
+
+        @JsOverlay
+        default Props onClick(final MouseEventHandler onClick) {
+            setOnClick(onClick);
+            return this;
+        }
 
 
     }
 
 
     @JsType(isNative = true)
-    public class BarData {
-        String name;
-        Values[] values;
+    public interface BarData {
+//        String name;
+//        Values[] values;
+
+        @JsProperty
+        String getName();
+
+        @JsProperty
+        void setName(String name);
+
+        @JsProperty
+        Values[] getValues();
+
+        @JsProperty
+        void setValues(Values[] values);
+
+        @JsOverlay
+        default BarData name(final String name) {
+            setName(name);
+            return this;
+        }
+        @JsOverlay
+        default BarData values(final Values[] values) {
+            setValues(values);
+            return this;
+        }
 
     }
 
     @JsType(isNative = true)
-    public class Values {
-        double x;
-        double y;
+    public interface Values {
+//        double x;
+//        double y;
+
+        @JsProperty
+        double getX();
+
+        @JsProperty
+        void setX(double x);
+
+        @JsProperty
+        double getY();
+
+        @JsProperty
+        void setY(double y);
+
+        @JsOverlay
+        default Values x(final double x) {
+            setX(x);
+            return this;
+        }
+
+        @JsOverlay
+        default Values y(final double y) {
+            setY(y);
+            return this;
+        }
+
     }
 
 
--- src/main/java/camber/client/reactd3/PieChart.java
+++ src/main/java/camber/client/reactd3/PieChart.java
@@ -4,56 +4,303 @@ package camber.client.reactd3;
 import io.clickhandler.web.Func;
 import io.clickhandler.web.event.MouseEventHandler;
 import io.clickhandler.web.react.BaseProps;
+import io.clickhandler.web.react.ExternalComponent;
+import io.clickhandler.web.react.ReactClass;
+import jsinterop.annotations.JsOverlay;
+import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
 
-public class PieChart {
+@Singleton
+public class PieChart extends ExternalComponent<PieChart.Props> {
 
     @Inject
     public PieChart() {
     }
 
-
+    @Override
+    protected native ReactClass<Props> reactClass() /*-{
+        return $wnd.Camber.PieChart;
+    }-*/;
 
     @JsType(isNative = true)
-    public class Props extends BaseProps {
-        PieData[] data;
-        String title;
-        double radius;
-        double innerRadius;
-        double cx;
-        double cy;
-        double width;
-        double height;
-        String labelTextFill;
-        String valueTextFill;
-        boolean showInnerLabels;
-        boolean showOuterLabels;
-        String sectorBorderColor;
-        boolean hoverAnimation;
+    public interface Props extends BaseProps {
+//        PieData[] data;
+//        String title;
+//        double radius;
+//        double innerRadius;
+//        double cx;
+//        double cy;
+//        double width;
+//        double height;
+//        String labelTextFill;
+//        String valueTextFill;
+//        boolean showInnerLabels;
+//        boolean showOuterLabels;
+//        String sectorBorderColor;
+//        boolean hoverAnimation;
+//        Func.Run colors;
+//        Func.Run2<Object, Object> colorAccessor;
+//        Func.Run1<Object> valueTextFormatter;
+//        MouseEventHandler onClick;
 
-        Func.Run colors;
-        Func.Run2<Object, Object> colorAccessor;
-        Func.Run1<Object> valueTextFormatter;
-        MouseEventHandler onClick;
-        
 
+        @JsProperty
+        PieData[] getData();
+
+        @JsProperty
+        void setData(PieData[] data);
+
+        @JsProperty
+        String getTitle();
+
+        @JsProperty
+        void setTitle(String title);
+
+        @JsProperty
+        double getRadius();
+
+        @JsProperty
+        void setRadius(double radius);
+
+        @JsProperty
+        double getInnerRadius();
+
+        @JsProperty
+        void setInnerRadius(double innerRadius);
+
+        @JsProperty
+        double getCx();
+
+        @JsProperty
+        void setCx(double cx);
+
+        @JsProperty
+        double getCy();
+
+        @JsProperty
+        void setCy(double cy);
+
+        @JsProperty
+        double getWidth();
+
+        @JsProperty
+        void setWidth(double width);
+
+        @JsProperty
+        double getHeight();
+
+        @JsProperty
+        void setHeight(double height);
+
+        @JsProperty
+        String getLabelTextFill();
+
+        @JsProperty
+        void setLabelTextFill(String labelTextFill);
+
+        @JsProperty
+        String getValueTextFill();
+
+        @JsProperty
+        void setValueTextFill(String valueTextFill);
+
+        @JsProperty
+        boolean isShowInnerLabels();
+
+        @JsProperty
+        void setShowInnerLabels(boolean showInnerLabels);
+
+        @JsProperty
+        boolean isShowOuterLabels();
+
+        @JsProperty
+        void setShowOuterLabels(boolean showOuterLabels);
+
+        @JsProperty
+        String getSectorBorderColor();
+
+        @JsProperty
+        void setSectorBorderColor(String sectorBorderColor);
+
+        @JsProperty
+        boolean isHoverAnimation();
+
+        @JsProperty
+        void setHoverAnimation(boolean hoverAnimation);
+
+        @JsProperty
+        Func.Run getColors();
+
+        @JsProperty
+        void setColors(Func.Run colors);
+
+        @JsProperty
+        Func.Run2<Object, Object> getColorAccessor();
+
+        @JsProperty
+        void setColorAccessor(Func.Run2<Object, Object> colorAccessor);
+
+        @JsProperty
+        Func.Run1<Object> getValueTextFormatter();
+
+        @JsProperty
+        void setValueTextFormatter(Func.Run1<Object> valueTextFormatter);
+
+        @JsProperty
+        MouseEventHandler getOnClick();
+
+        @JsProperty
+        void setOnClick(MouseEventHandler onClick);
 
 
         ////////////////////
         // fluent setters
         ////////////////////
 
+        @JsOverlay
+        default Props data(final PieData[] data) {
+            setData(data);
+            return this;
+        }
+
+        @JsOverlay
+        default Props title(final String title) {
+            setTitle(title);
+            return this;
+        }
+
+        @JsOverlay
+        default Props radius(final double radius) {
+            setRadius(radius);
+            return this;
+        }
+
+        @JsOverlay
+        default Props innerRadius(final double innerRadius) {
+            setInnerRadius(innerRadius);
+            return this;
+        }
+
+        @JsOverlay
+        default Props cx(final double cx) {
+            setCx(cx);
+            return this;
+        }
+
+        @JsOverlay
+        default Props cy(final double cy) {
+            setCy(cy);
+            return this;
+        }
+
+        @JsOverlay
+        default Props width(final double width) {
+            setWidth(width);
+            return this;
+        }
+
+        @JsOverlay
+        default Props height(final double height) {
+            setHeight(height);
+            return this;
+        }
+
+        @JsOverlay
+        default Props labelTextFill(final String labelTextFill) {
+            setLabelTextFill(labelTextFill);
+            return this;
+        }
+
+        @JsOverlay
+        default Props valueTextFill(final String valueTextFill) {
+            setValueTextFill(valueTextFill);
+            return this;
+        }
+
+        @JsOverlay
+        default Props showInnerLabels(final boolean showInnerLabels) {
+            setShowInnerLabels(showInnerLabels);
+            return this;
+        }
+
+        @JsOverlay
+        default Props showOuterLabels(final boolean showOuterLabels) {
+            setShowOuterLabels(showOuterLabels);
+            return this;
+        }
+
+        @JsOverlay
+        default Props sectorBorderColor(final String sectorBorderColor) {
+            setSectorBorderColor(sectorBorderColor);
+            return this;
+        }
+
+        @JsOverlay
+        default Props hoverAnimation(final boolean hoverAnimation) {
+            setHoverAnimation(hoverAnimation);
+            return this;
+        }
+
+        @JsOverlay
+        default Props colors(final Func.Run colors) {
+            setColors(colors);
+            return this;
+        }
+
+        @JsOverlay
+        default Props colorAccessor(final Func.Run2<Object, Object> colorAccessor) {
+            setColorAccessor(colorAccessor);
+            return this;
+        }
+
+        @JsOverlay
+        default Props valueTextFormatter(final Func.Run1<Object> valueTextFormatter) {
+            setValueTextFormatter(valueTextFormatter);
+            return this;
+        }
+
+        @JsOverlay
+        default Props onClick(final MouseEventHandler onClick) {
+            setOnClick(onClick);
+            return this;
+        }
 
 
     }
 
 
     @JsType(isNative = true)
-    public class PieData {
-        String label;
-        double value;
+    public interface PieData {
+//        String label;
+//        double value;
+
+        @JsProperty
+        String getLabel();
+
+        @JsProperty
+        void setLabel(String label);
+
+        @JsProperty
+        double getValue();
+
+        @JsProperty
+        void setValue(double value);
+
+        @JsOverlay
+        default PieData label(final String label) {
+            setLabel(label);
+            return this;
+        }
+
+        @JsOverlay
+        default PieData value(final double value) {
+            setValue(value);
+            return this;
+        }
+
 
     }
 
