2beecee777f6c7e4812fce7c02700ac733d829f0
Chris Jones <chris@axismedtech.com>
2016-04-11 14:00:15 -0600

    lodash update

--- src/main/java/io/clickhandler/camber/client/util/Lodash.java
+++ src/main/java/io/clickhandler/camber/client/util/Lodash.java
@@ -1,10 +1,11 @@
 package io.clickhandler.camber.client.util;
 
+import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
 
-@JsType(isNative = true, namespace = "Camber")
+@JsType(isNative = true, namespace = JsPackage.GLOBAL)
 public class Lodash {
-    public static native Object assign(Object object, Object ... sources);
+    public static native Object assign(Object object, Object... sources);
 
     public static native boolean isEqual(Object o1, Object o2);
 }
